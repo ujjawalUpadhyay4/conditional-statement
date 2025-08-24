562327f356c098209c057712b99a7128881f2d73
ujjawalUpadhyay4 <96589441+ujjawalUpadhyay4@users.noreply.github.com>
2025-08-24 22:37:27 +0530

    Update average.py

--- control_statement/average.py
+++ control_statement/average.py
@@ -1,1 +1,7 @@
-
+a = int(input("enter first number"))
+b = int(input("enter second number"))
+c = int(input("enter third number"))
+d = int(input("enter fourth number"))
+e = int(input("enter fifth number"))
+average = (a+b+c+d+e)/5
+print("the average is: ",average)
